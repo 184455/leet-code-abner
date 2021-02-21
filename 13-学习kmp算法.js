e49821d258198c05dbdf6dbbb7d8a94da25ab9df
19055 <zhongji.huang@hand-china.com>
2021-02-21 16:30:45 +0800

    feat: 完成字符串的 sunday 算法

--- 13-学习kmp算法.js
+++ 13-学习kmp算法.js
@@ -34,7 +34,7 @@ function forcePatter (s, p) {
 
 // const s = 'BBC ABCDAB ABCDABCDABDE'
 // const p = 'ABCDABD'
-// console.log(forcePatter(s, p));
+// console.log(forcePatter(s, p))
 
 /**
  * kmp 字符串匹配算法
@@ -43,38 +43,38 @@ function forcePatter (s, p) {
  */
 function kmp (s, p) {
   function getNext (p) {
-    const next = [-1];
-    const pLen = p.length - 1;
-    let k = -1;
-    let j = 0;
+    const next = [-1]
+    const pLen = p.length - 1
+    let k = -1
+    let j = 0
 
     while (j < pLen) {
       // p[k]表示前缀，p[j]表示后缀
       if (k === -1 || p[k] === p[j]) {
-        ++k;
-        ++j;
+        ++k
+        ++j
 
         // 因为不能出现 p[j] = p[next[j]]，所以当出现时需要继续递归，k = next[k] = next[next[k]]
         // 其实就是防止：abab 这样的模式字符串
-        next[j] = p[k] !== p[j] ? k : next[k];
+        next[j] = p[k] !== p[j] ? k : next[k]
       } else {
-        k = next[k];
+        k = next[k]
       }
     }
 
-    return next;
+    return next
   }
-  const sLen = s.length;
-  const pLen = p.length;
-  const next = getNext(p);
-  let i = 0;
-  let j = 0;
+  const sLen = s.length
+  const pLen = p.length
+  const next = getNext(p)
+  let i = 0
+  let j = 0
 
   while (i < sLen && j < pLen) {
     if (j === -1 || s[i] === p[j]) {
       // j === -1 或 字符匹配，继续寻找下一个匹配
-      i++;
-      j++;
+      i++
+      j++
     } else {
       // 如果 j !== -1，且当前字符匹配失败（即S[i] !== P[j]），则令 i 不变，j = next[j]    
 			// next[j] 即为 j 所对应的next值
@@ -85,6 +85,55 @@ function kmp (s, p) {
   return j === pLen ? i - j : -1
 }
 
+// https://baike.baidu.com/item/sunday%20%E7%AE%97%E6%B3%95?fr=aladdin
+/**
+最快的字符串匹配算法：Sunday 算法
+核心思想：从前往后匹配，当匹配失败时，关心的是匹配串末尾的下一个字符（设末尾字符为 k）。
+         如果：末尾字符 k 没有在模式串中出现过，则移动长度 = 匹配串长度 + 1
+         否则：其移动位数 = 模式串中最右端的该字符到末尾的距离 + 1
+ */
+function sundayMatching (s, p) {
+  const sLen = s.length
+  const pLen = p.length
+  let pNext = 0 // 匹配串末尾的下一个字符
+  let i = 0
+  let j = 0
+
+  // 序列化匹配串
+  let index = 0
+  const mapping = {}
+  while (index < pLen) {
+    mapping[p[index]] = index
+    index++
+  }
+
+  while (i < sLen && j < pLen) {
+    if (s[i] === p[j]) {
+      i++
+      j++
+    } else {
+      // s 和 p 失配
+      pNext = i - j + pLen
+      if (mapping[s[pNext]] !== undefined) {
+        //【匹配串末尾的下一个字符】在匹配串中出现过
+        i = pNext - mapping[s[pNext]]
+      } else {
+        // 不在匹配串中，移动到【匹配串的长度 + 1】个长度
+        i = pNext + 1
+      }
+      j = 0
+    }
+  }
+
+  return j === pLen ? i - j : -1
+}
+
+// const s = 'BBC ABCDAB ABCDABCDABDE'
+// const p = 'ABCDABD'
+// const s = 'substring searching algorithm'
+// const p = 'search'
 const s = 'abacababc'
 const p = 'abab'
-console.log(kmp(s, p));
+// const s = 'sss'
+// const p = 'sss'
+console.log(kmp(s, p))
