a89a41e028634147c05695f450824f76db2ecc7c
19055 <zhongji.huang@hand-china.com>
2021-04-29 20:41:48 +0800

    feat: 更新树的遍历

--- 41-94. 二叉树的中序遍历.js
+++ 41-94. 二叉树的中序遍历.js
@@ -6,19 +6,45 @@
  *     this.right = (right===undefined ? null : right)
  * }
  */
-/**
- * @param {TreeNode} root
- * @return {number[]}
- */
- var inorderTraversal = function(root) {
-  const res = []
-  const foreachTree = (node) => {
-      if (!node) { return }
-      foreachTree(node.left)
-      res.push(node.val)
-      foreachTree(node.right)
-  }
-  foreachTree(root)
 
+// 先序遍历
+const preTraversal = (root) => {
+  const res = []
+  const preOrder = (root) => {
+    if (!root) { return }
+    res.push(root.val)
+    preOrder(root.left)
+    preOrder(root.right)
+  }
+
+  preOrder(root)
   return res
-};
+}
+
+ // 中序遍历
+ const inTraversal = (root) => {
+   const res = []
+   const inOrder = (root) => {
+     if (!root) { return }
+     inOrder(root.left)
+     res.push(root.val)
+     inOrder(root.right)
+   }
+
+   inOrder(root)
+   return res
+ }
+
+ // 后序遍历
+ const afterTraversal = (root) => {
+   const res = []
+   const afterOrder = (root) => {
+     if (!root) { return }
+     afterOrder(root.left)
+     afterOrder(root.right)
+     res.push(root.val)
+   }
+
+   afterOrder(root)
+   return res
+ }
